6e20a3fe0f5c1ab0655f5c32185ec4a82648259c
LuandersonH <luandersonfamilia@gmail.com>
2026-01-12 21:11:30 -0300

    PUT

--- Petfolio.Application/UseCases/Pets/Register/RegisterPetUseCase.cs
+++ Petfolio.Application/UseCases/Pets/Register/RegisterPetUseCase.cs
@@ -5,16 +5,11 @@ namespace Petfolio.Application.UseCases.Pets.Register;
 
 public class RegisterPetUseCase
 {
-    public ResponseRegisteredPetJson Execute(ResponseRegisteredPetJson request) {
+    public ResponseRegisteredPetJson Execute(RequestPetJson request) {
         return new ResponseRegisteredPetJson
         {
             Id = 7,
             Name = request.Name
         };
-    }
-
-    public object Execute(RequestRegisterPetJson request)
-    {
-        throw new NotImplementedException();
     }
 }
